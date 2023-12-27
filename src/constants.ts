d3921294632b657377932dee544c72deed734036
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-28 04:32:02 +0530

    Created constants.ts for storing price plan data

---
+++ src/constants.ts
@@ -0,0 +1,29 @@
+  // Pricing plans data
+export const plans = [
+    {
+      id: 'basic',
+      planName: 'Basic Plan',
+      price: '$4/month/user',
+      features: ['Up to 10 graphs', 'Up to 500 chatbot queries'],
+    },
+    {
+      id: 'pro',
+      planName: 'Pro Plan',
+      price: '$8/month/user',
+      features: [
+        'Up to 50 graphs',
+        'Up to 1000 chatbot queries',
+        'Email + live chat support',
+      ],
+    },
+    {
+      id: 'enterprise',
+      planName: 'Enterprise Plan',
+      price: 'Contact for price',
+      features: [
+        'Unlimited graphs',
+        'Ability to upload custom PDFs',
+        '24/7 phone + email support',
+      ],
+    },
+  ];
