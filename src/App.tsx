ee17725aa004253fb156f558a8e12fb006f802ee
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-30 23:20:43 +0530

    Made necessary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PricingSection from './Components/Pricing/PricingSection';
+import PricingSection from './Pages/PricingSection';
+import ProfileDashboard from './Pages/ProfileDashboard';
+import CreateNewGraph from './Pages/CreateNewGraph';
+import GraphDetailsPage from './Pages/GraphDetailsPage';
+import AccountSettings from './Pages/AccountSettings';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
+          {/* Route for Pricing Section */}
           <Route path="/pricing" element={<PricingSection />} />
+
+          {/* Route for Profile Dashboard */}
+          <Route path="/profile">
+            <Route index element={<ProfileDashboard />} />
+            <Route path="/profile/new-graph" element={<CreateNewGraph />} />
+            {/* <Route path="/profile/graph_details" element={<GraphDetailsPage />} />
+            <Route path="/profile/settings" element={<AccountSettings />} /> */}
+          </Route>
         </Routes>
       </div>
     </Router>
---
+++ src/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import Sidebar from './Components/Sidebar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
